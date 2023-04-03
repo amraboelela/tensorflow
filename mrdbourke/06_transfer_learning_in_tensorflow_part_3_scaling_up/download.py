90a2b4097fe712cbc0843abf107255046aae526d
Amr Aboelela <amraboelela@gmail.com>
2023-04-02 21:11:37 -0700

    data

--- mrdbourke/06_transfer_learning_in_tensorflow_part_3_scaling_up/download.py
+++ mrdbourke/06_transfer_learning_in_tensorflow_part_3_scaling_up/download.py
@@ -1,10 +1,12 @@
+from os import path
 import subprocess
 
-subprocess.run(['wget', 'https://raw.githubusercontent.com/mrdbourke/tensorflow-deep-learning/main/extras/helper_functions.py'])
-subprocess.run(['wget', 'https://storage.googleapis.com/ztm_tf_course/food_vision/101_food_classes_10_percent.zip'])
+if not path.exists('helper_functions.py'):
+    subprocess.run(['wget', 'https://raw.githubusercontent.com/mrdbourke/tensorflow-deep-learning/main/extras/helper_functions.py'])
+if not path.exists('data/' + '101_food_classes_10_percent'):
+    subprocess.run(['rm', '101_food_classes_10_percent.zip'])
+    subprocess.run(['wget', 'https://storage.googleapis.com/ztm_tf_course/food_vision/101_food_classes_10_percent.zip'])
 subprocess.run(['unzip', '101_food_classes_10_percent'])
 subprocess.run(['mkdir', '-p', 'data'])
 subprocess.run(['mv', '101_food_classes_10_percent', 'data'])
-#unzip_data("101_food_classes_10_percent.zip")
 subprocess.run(['rm', '101_food_classes_10_percent.zip'])
-walk_through_dir("data/101_food_classes_10_percent")
---
+++ mrdbourke/06_transfer_learning_in_tensorflow_part_3_scaling_up/test_data.py
@@ -0,0 +1,3 @@
+from helper_functions import *
+
+walk_through_dir("data/101_food_classes_10_percent")
