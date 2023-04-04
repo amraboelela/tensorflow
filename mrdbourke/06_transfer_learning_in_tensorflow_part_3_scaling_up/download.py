41e0bb165cf3076dff265ba96ec5faa74d9f246a
Amr Aboelela <amraboelela@gmail.com>
2023-04-04 10:18:40 -0700

    Update download.py

--- mrdbourke/06_transfer_learning_in_tensorflow_part_3_scaling_up/download.py
+++ mrdbourke/06_transfer_learning_in_tensorflow_part_3_scaling_up/download.py
@@ -2,13 +2,18 @@ from os import path
 import subprocess
 from helper_functions import *
 
-if not path.exists('data/' + '101_food_classes_10_percent'):
-    subprocess.run(['rm', '101_food_classes_10_percent.zip'])
-    subprocess.run(['wget', 'https://storage.googleapis.com/ztm_tf_course/food_vision/101_food_classes_10_percent.zip'])
-subprocess.run(['unzip', '101_food_classes_10_percent'])
+def download_resource(resource):
+    if not path.exists('data/' + resource):
+        subprocess.run(['rm', resource + '.zip'])
+        subprocess.run(['wget', 'https://storage.googleapis.com/ztm_tf_course/food_vision/' + resource + '.zip'])
+        subprocess.run(['unzip', resource])
+        subprocess.run(['mv', resource, 'data'])
+        subprocess.run(['rm', resource + '.zip'])
+        subprocess.run(['rm', '-r', '__MACOSX'])
+    
 subprocess.run(['mkdir', '-p', 'data'])
-subprocess.run(['mv', '101_food_classes_10_percent', 'data'])
-subprocess.run(['rm', '101_food_classes_10_percent.zip'])
-subprocess.run(['rm', '-r', '__MACOSX'])
+download_resource('101_food_classes_10_percent')
 walk_through_dir("data/101_food_classes_10_percent")
+
+download_resource('custom_food_images')
 
