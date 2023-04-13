3912623b1a90fac0498eebaf0f94bc4eebff0881
Amr Aboelela <amraboelela@gmail.com>
2023-04-12 18:18:04 -0700

    Update common.py

--- mrdbourke/08_introduction_to_nlp_in_tensorflow/common.py
+++ mrdbourke/08_introduction_to_nlp_in_tensorflow/common.py
@@ -17,6 +17,10 @@ train_df = pd.read_csv("data/train.csv")
 test_df = pd.read_csv("data/test.csv")
 print(str(train_df.head()))
 
+# Shuffle training dataframe
+train_df_shuffled = train_df.sample(frac=1, random_state=42) # shuffle with random_state=42 for reproducibility
+print(str(train_df_shuffled.head()))
+
 imagePath = "/home/amr/webserver/HaneinWebserver/Public/images"
 print("")
 print("")
