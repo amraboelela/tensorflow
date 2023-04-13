4e1d98ed93dc6233a051c712dcfe23b9a8c5367a
Amr Aboelela <amraboelela@gmail.com>
2023-04-12 18:11:35 -0700

    Update common.py

--- mrdbourke/08_introduction_to_nlp_in_tensorflow/common.py
+++ mrdbourke/08_introduction_to_nlp_in_tensorflow/common.py
@@ -11,6 +11,12 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 import random
 
+# Turn .csv files into pandas DataFrame's
+import pandas as pd
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
+train_df.head()
+
 imagePath = "/home/amr/webserver/HaneinWebserver/Public/images"
 print("")
 print("")
