c1b160a49ad94cf142c3ea9e3e93436962bf1c8b
Amr Aboelela <amraboelela@gmail.com>
2023-04-12 20:16:15 -0700

    Update data_analyze.py

--- mrdbourke/08_introduction_to_nlp_in_tensorflow/data_analyze.py
+++ mrdbourke/08_introduction_to_nlp_in_tensorflow/data_analyze.py
@@ -2,13 +2,16 @@ from common import *
 
 print("train_df.head()")
 print(str(train_df.head()))
+print("")
 
 # Shuffle training dataframe
 train_df_shuffled = train_df.sample(frac=1, random_state=42) # shuffle with random_state=42 for reproducibility
 print("train_df_shuffled.head()")
 print(str(train_df_shuffled.head()))
+print("")
 
 # The test data doesn't have a target (that's what we'd try to predict)
 print("test_df.head()")
 print(str(test_df.head()))
+print("")
 
