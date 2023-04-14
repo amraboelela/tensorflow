15992236f14bdbc0d0113e3efde6688a6fcbecc1
Amr Aboelela <amraboelela@gmail.com>
2023-04-13 18:54:58 -0700

    Update common.py

--- mrdbourke/08_introduction_to_nlp_in_tensorflow/common.py
+++ mrdbourke/08_introduction_to_nlp_in_tensorflow/common.py
@@ -22,10 +22,13 @@ test_df = pd.read_csv("data/test.csv")
 train_df_shuffled = train_df.sample(frac=1, random_state=42) # shuffle with random_state=42 for reproducibility
 
 # Use train_test_split to split training data into training and validation sets
-train_sentences, val_sentences, train_labels, val_labels = train_test_split(train_df_shuffled["text"].to_numpy(),
+train_sentences, val_sentences, train_labels, val_labels =
+    train_test_split(
+        train_df_shuffled["text"].to_numpy(),
         train_df_shuffled["target"].to_numpy(),
         test_size=0.1, # dedicate 10% of samples to validation set
-        andom_state=42) # random state for reproducibility
+        random_state=42 # random state for reproducibility
+    )
                                                                             
 imagePath = "/home/amr/webserver/HaneinWebserver/Public/images"
 print("")
