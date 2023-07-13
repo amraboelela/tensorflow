dd8b50d39966d29c2b245822a104ad22d63d0c2a
Amr Aboelela <amraboelela@gmail.com>
2023-07-13 10:03:10 -0700

    03

---
+++ mrdbourke/03_convolutional_neural_networks_in_tensorflow/data_analyze.py
@@ -0,0 +1,43 @@
+from common import *
+
+print("train_df.head()")
+print(str(train_df.head()))
+print("")
+
+print("train_df_shuffled.head()")
+print(str(train_df_shuffled.head()))
+print("")
+
+# The test data doesn't have a target (that's what we'd try to predict)
+print("test_df.head()")
+print(str(test_df.head()))
+print("")
+
+# How many examples of each class?
+print("train_df.target.value_counts()")
+print(str(train_df.target.value_counts()))
+print("")
+
+# How many samples total?
+print(f"Total training samples: {len(train_df)}")
+print(f"Total test samples: {len(test_df)}")
+print(f"Total samples: {len(train_df) + len(test_df)}")
+print("")
+
+random_index = random.randint(0, len(train_df)-5) # create random indexes not higher than the total number of samples
+for row in train_df_shuffled[["text", "target"]][random_index:random_index+5].itertuples():
+  _, text, target = row
+  print(f"Target: {target}", "(real disaster)" if target > 0 else "(not real disaster)")
+  print(f"Text:\n{text}\n")
+  print("---\n")
+print("")
+
+# Check the lengths
+print("len(train_sentences), len(train_labels), len(val_sentences), len(val_labels) :")
+print(f"{len(train_sentences)}, {len(train_labels)}, {len(val_sentences)}, {len(val_labels)}")
+print("")
+
+# View the first 10 training sentences and their labels
+print("train_sentences[:10], train_labels[:10]: ")
+print(f"{train_sentences[:10]}, {train_labels[:10]}")
+print("")
---
+++ mrdbourke/03_convolutional_neural_networks_in_tensorflow/download.py
@@ -0,0 +1,6 @@
+from helper_functions import *
+
+download_resource('nlp_getting_started')
+#walk_through_dir("data/nlp_getting_started")
+print("")
+
