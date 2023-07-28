986b5ea1c512c26b6091f5522b073803925d9d92
Amr Aboelela <amraboelela@gmail.com>
2023-07-28 16:52:20 -0700

    model1_evaluate

--- mrdbourke/06_transfer_learning3/model1_load.py
+++
@@ -1,8 +0,0 @@
-from model1_init import *
-
-model.load_weights('data/model1.keras')
-
-# Load the saved history object from a file
-with open('data/history_all_classes_10_percent.pkl', 'rb') as f:
-    history_all_classes_10_percent = pickle.load(f)
-
--- mrdbourke/07_food_vision1/download.py
+++
@@ -1,9 +0,0 @@
-from common import *
-    
-# Load in the data (takes about 5-6 minutes in Google Colab)
-(train_data, test_data), ds_info = tfds.load(name="food101", # target dataset to get from TFDS
-                                             split=["train", "validation"], # what splits of data should we get? note: not all datasets have train, valid, test
-                                             shuffle_files=True, # shuffle files on download?
-                                             as_supervised=True, # download data in tuple format (sample, label), e.g. (image, label)
-                                             with_info=True) # include dataset metadata? if so, tfds.load() returns tuple (data, ds_info)
-
