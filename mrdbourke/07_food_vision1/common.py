e365d555135ea10035f966631805f0dc18e121b8
Amr Aboelela <amraboelela@gmail.com>
2023-08-19 14:33:35 -0700

    07 common.py

--- mrdbourke/07_food_vision1/common.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('../modules')
-from helper_functions import *
-
-# List available datasets
-datasets_list = tfds.list_builders() # get all available datasets in TFDS
-print("food101" in datasets_list) # is the dataset we're after available?
